854ce821476fb9c08fe3bd20320357463b9b8d6f
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-07-05 16:03:42 +0800

    fix: deploy for heroku

--- commands/scraptest.js
+++ commands/scraptest.js
@@ -24,47 +24,47 @@ module.exports = {
     async execute(interaction) {
         let randomFact;
         let healthArray = [];
-        (async () => {
-            // console.log("Im in");
-            const browser = await puppeteer.launch();
 
-            const page = await browser.newPage();
-            await page.goto("https://www.thegoodbody.com/health-facts/");
+        const browser = await puppeteer.launch();
 
-            await page.screenshot({ path: "image.png" });
+        const page = await browser.newPage();
+        await page.goto("https://www.thegoodbody.com/health-facts/");
 
-            const pageData = await page.evaluate(() => {
-                return {
-                    html: document.documentElement.innerHTML,
-                };
+        //await page.screenshot({ path: "image.png" });
+
+        const pageData = await page.evaluate(() => {
+            return {
+                html: document.documentElement.innerHTML,
+            };
+        });
+
+        //console.log(pageData);
+
+        const $ = cheerio.load(pageData.html);
+        //const element = $()
+        $(".round-number")
+            .find("h3")
+            .each(function (i, el) {
+                let row = $(el).text().replace(/(\s+)/g, " ");
+                row = $(el)
+                    .text()
+                    .replace(/[0-9]+. /g, "")
+                    .trim();
+                console.log(`${row}`);
+                healthArray.push(row);
             });
 
-            //console.log(pageData);
+        await browser.close();
+        // console.log("Im out");
+        console.log("healthArray: ", healthArray);
+        console.log("total array: ", healthArray.length);
+        randomFact =
+            healthArray[
+                Math.floor(Math.random() * healthArray.length)
+            ].toString();
+        console.log(`Random Health Fact: ${randomFact}`);
+        //interaction.channel.send(randomFact);
 
-            const $ = cheerio.load(pageData.html);
-            //const element = $()
-            $(".round-number")
-                .find("h3")
-                .each(function (i, el) {
-                    let row = $(el).text().replace(/(\s+)/g, " ");
-                    row = $(el)
-                        .text()
-                        .replace(/[0-9]+. /g, "")
-                        .trim();
-                    console.log(`${row}`);
-                    healthArray.push(row);
-                });
-
-            await browser.close();
-            // console.log("Im out");
-            console.log("healthArray: ", healthArray);
-            console.log("total array: ", healthArray.length);
-            randomFact =
-                healthArray[Math.floor(Math.random() * healthArray.length)];
-            console.log(`Random Health Fact: ${randomFact}`);
-            //interaction.channel.send(randomFact);
-            
-        })();
         await interaction.reply(randomFact);
     },
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 //const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+// const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -21,8 +21,8 @@ for (const file of commandFiles) {
 ]
 	.map(command => command.toJSON());*/
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(process.env.token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(process.env.clientId, process.env.guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
