38169ea9ef52917e21bf367e1ede08ecb7519d8d
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-07-05 16:03:42 +0800

    fix: deploy for heroku

--- commands/healthfactscrape.js
+++ commands/healthfactscrape.js
@@ -10,50 +10,47 @@ module.exports = {
     async execute(interaction) {
         let randomFact;
         let healthArray = [];
-        (async () => {
-            const browser = await puppeteer.launch({
-                headless: true,
-                args: ["--no-sandbox", "--disable-setuid-sandbox"],
+
+        const browser = await puppeteer.launch();
+
+        const page = await browser.newPage();
+        await page.goto("https://www.thegoodbody.com/health-facts/");
+
+        //await page.screenshot({ path: "image.png" });
+
+        const pageData = await page.evaluate(() => {
+            return {
+                html: document.documentElement.innerHTML,
+            };
+        });
+
+        //console.log(pageData);
+
+        const $ = cheerio.load(pageData.html);
+        //const element = $()
+        $(".round-number")
+            .find("h3")
+            .each(function (i, el) {
+                let row = $(el).text().replace(/(\s+)/g, " ");
+                row = $(el)
+                    .text()
+                    .replace(/[0-9]+. /g, "")
+                    .trim();
+                console.log(`${row}`);
+                healthArray.push(row);
             });
 
-            const page = await browser.newPage();
-            await page.goto("https://www.thegoodbody.com/health-facts/");
+        await browser.close();
+        // console.log("Im out");
+        console.log("healthArray: ", healthArray);
+        console.log("total array: ", healthArray.length);
+        randomFact =
+            healthArray[
+                Math.floor(Math.random() * healthArray.length)
+            ].toString();
+        console.log(`Random Health Fact: ${randomFact}`);
+        //interaction.channel.send(randomFact);
 
-            //await page.screenshot({ path: "image.png" });
-
-            const pageData = await page.evaluate(() => {
-                return {
-                    html: document.documentElement.innerHTML,
-                };
-            });
-
-
-
-            const $ = cheerio.load(pageData.html);
-            //const element = $()
-            $(".round-number")
-                .find("h3")
-                .each(function (i, el) {
-                    let row = $(el).text().replace(/(\s+)/g, " ");
-                    row = $(el)
-                        .text()
-                        .replace(/[0-9]+. /g, "")
-                        .trim();
-
-                    healthArray.push(row);
-                });
-
-            await browser.close();
-
-
-            randomFact =
-                healthArray[
-                    Math.floor(Math.random() * healthArray.length)
-                ].toString();
-
-            interaction.channel.send(randomFact);
-        })();
-
-        await interaction.reply("Health Fact:");
+        await interaction.reply(randomFact);
     },
 };
