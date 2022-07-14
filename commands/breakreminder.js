6c884670a73ebad31b7be491f96aa8762909a26e
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-07-15 04:28:27 +0800

    fix: aiyok

--- commands/breakreminder.js
+++ commands/breakreminder.js
@@ -115,7 +115,7 @@ module.exports = {
                 );
 
                 let userdbStop = await User.findOne({discordId: currentUser}).select("-_id reminderStop");
-                let userDbStopValue = userdbStop[0];
+                let userDbStopValue = userdbStop.reminderStop;
                 console.log("userdbStop: ", userDbStopValue);
 
                 // check timer validation
