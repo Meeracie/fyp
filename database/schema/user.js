4da5df615939d261214fa975ce43857caed25ffe
Taufiq Zarif <taufiqzarif90@gmail.com>
2022-07-15 03:40:29 +0800

    fix: changed in user.js

--- database/schema/user.js
+++ database/schema/user.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     username: { type: mongoose.SchemaTypes.String, required: true },
     discordId: { type: mongoose.SchemaTypes.String, required: true },
     reminder: { type: mongoose.SchemaTypes.String },
-    stop: { type: mongoose.SchemaTypes.Boolean },
-    ongoing: { type: mongoose.SchemaTypes.Boolean},
+    reminderStop: { type: mongoose.SchemaTypes.Boolean },
+    reminderOngoing: { type: mongoose.SchemaTypes.Boolean},
 });
 
 module.exports = mongoose.model("User", userSchema);
